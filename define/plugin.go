2014a96ca2922373840afbdc991ea3c52f734397
pangdogs <pangdogs@gmail.com>
2022-10-20 21:59:59 +0800

    整理代码

--- define/plugin.go
+++ define/plugin.go
@@ -7,46 +7,46 @@ import (
 	"github.com/pangdogs/galaxy/util"
 )
 
-type _Plugin[PT, OPT any] struct {
+type _Plugin[PLUGIN, OPTION any] struct {
 	name string
 }
 
 // Name 生成插件名称
-func (p _Plugin[PT, OPT]) Name() string {
+func (p _Plugin[PLUGIN, OPTION]) Name() string {
 	return p.name
 }
 
 // Register 生成插件注册函数
-func (p _Plugin[PT, OPT]) Register(creator func(...OPT) PT) func(plugin.PluginLib, ...OPT) {
-	return func(lib plugin.PluginLib, options ...OPT) {
-		plugin.RegisterPlugin[PT](lib, p.Name(), creator(options...))
+func (p _Plugin[PLUGIN, OPTION]) Register(creator func(...OPTION) PLUGIN) func(plugin.PluginLib, ...OPTION) {
+	return func(lib plugin.PluginLib, options ...OPTION) {
+		plugin.RegisterPlugin[PLUGIN](lib, p.Name(), creator(options...))
 	}
 }
 
 // Deregister 生成插件取消注册函数
-func (p _Plugin[PT, OPT]) Deregister() func(plugin.PluginLib) {
+func (p _Plugin[PLUGIN, OPTION]) Deregister() func(plugin.PluginLib) {
 	return func(lib plugin.PluginLib) {
 		lib.Deregister(p.Name())
 	}
 }
 
 // ServiceGet 生成从服务上下文中获取插件函数
-func (p _Plugin[PT, OPT]) ServiceGet() func(service.Context) PT {
-	return func(ctx service.Context) PT {
-		return service.Plugin[PT](ctx, p.Name())
+func (p _Plugin[PLUGIN, OPTION]) ServiceGet() func(service.Context) PLUGIN {
+	return func(ctx service.Context) PLUGIN {
+		return service.Plugin[PLUGIN](ctx, p.Name())
 	}
 }
 
 // RuntimeGet 生成从运行时上下文中获取插件函数
-func (p _Plugin[PT, OPT]) RuntimeGet() func(runtime.Context) PT {
-	return func(ctx runtime.Context) PT {
-		return runtime.Plugin[PT](ctx, p.Name())
+func (p _Plugin[PLUGIN, OPTION]) RuntimeGet() func(runtime.Context) PLUGIN {
+	return func(ctx runtime.Context) PLUGIN {
+		return runtime.Plugin[PLUGIN](ctx, p.Name())
 	}
 }
 
 // Plugin 用于定义插件
-func Plugin[PT, OPT any]() _Plugin[PT, OPT] {
-	return _Plugin[PT, OPT]{
-		name: util.TypeFullName[PT](),
+func Plugin[PLUGIN, OPTION any]() _Plugin[PLUGIN, OPTION] {
+	return _Plugin[PLUGIN, OPTION]{
+		name: util.TypeFullName[PLUGIN](),
 	}
 }
