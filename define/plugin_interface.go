add9cd149bcb0a3f70bd17ffbf4af5ae7a8ccbb9
pangdogs <pangdogs@gmail.com>
2022-10-25 00:34:57 +0800

    整理代码

--- define/plugin_interface.go
+++ define/plugin_interface.go
@@ -1,7 +1,6 @@
 package define
 
 import (
-	"github.com/pangdogs/galaxy/ec"
 	"github.com/pangdogs/galaxy/runtime"
 	"github.com/pangdogs/galaxy/service"
 	"github.com/pangdogs/galaxy/util"
@@ -32,35 +31,29 @@ func (p _PluginInterface[PLUGIN]) RuntimeGet() func(runtime.Context) PLUGIN {
 
 // ServicePluginInterface 服务类插件接口
 type ServicePluginInterface[PLUGIN any] struct {
-	Name  string
-	Get   func(service.Context) PLUGIN
-	ECGet func(ec.ContextHolder) PLUGIN
+	Name string
+	Get  func(service.Context) PLUGIN
 }
 
 // ServicePluginInterface 生成服务类插件接口定义
 func (p _PluginInterface[PLUGIN]) ServicePluginInterface() ServicePluginInterface[PLUGIN] {
-	get := p.ServiceGet()
 	return ServicePluginInterface[PLUGIN]{
-		Name:  p.Name(),
-		Get:   get,
-		ECGet: func(ctxHolder ec.ContextHolder) PLUGIN { return get(service.Get(ctxHolder)) },
+		Name: p.Name(),
+		Get:  p.ServiceGet(),
 	}
 }
 
 // RuntimePluginInterface 运行时类插件接口
 type RuntimePluginInterface[PLUGIN any] struct {
-	Name  string
-	Get   func(runtime.Context) PLUGIN
-	ECGet func(ec.ContextHolder) PLUGIN
+	Name string
+	Get  func(runtime.Context) PLUGIN
 }
 
 // RuntimePluginInterface 生成运行时类插件接口定义
 func (p _PluginInterface[PLUGIN]) RuntimePluginInterface() RuntimePluginInterface[PLUGIN] {
-	get := p.RuntimeGet()
 	return RuntimePluginInterface[PLUGIN]{
-		Name:  p.Name(),
-		Get:   get,
-		ECGet: func(ctxHolder ec.ContextHolder) PLUGIN { return get(runtime.Get(ctxHolder)) },
+		Name: p.Name(),
+		Get:  p.RuntimeGet(),
 	}
 }
 
