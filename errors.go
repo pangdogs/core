edea36d14fe7993c5f2cf4cf219fb24348e8ff92
pangdogs <pangdogs@gmail.com>
2023-09-20 17:13:23 +0800

    更新注释

--- errors.go
+++ errors.go
@@ -8,7 +8,7 @@ import (
 var (
 	ErrGolaxy   = internal.ErrGolaxy                   // GOLAXY框架错误
 	ErrPanicked = internal.ErrPanicked                 // panic错误
-	ErrArgs     = internal.ErrArgs                     // 参数类错误
+	ErrArgs     = internal.ErrArgs                     // 参数错误
 	ErrRuntime  = fmt.Errorf("%w: runtime", ErrGolaxy) // 运行时错误
 	ErrService  = fmt.Errorf("%w: service", ErrGolaxy) // 服务错误
 )
