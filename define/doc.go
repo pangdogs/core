0f2d49168f1ce7af2ec27573b3078f6e0cf9db47
pangdogs <pangdogs@gmail.com>
2024-01-24 13:45:01 +0800

    更新代码

--- define/doc.go
+++ define/doc.go
@@ -1,7 +1,7 @@
 // Package define 利用泛型特性，简化代码编写。
 /*
-   - 支持组件与组件接口定义，可以参考官方组件库：https://github.com/pangdogs/components，学习如何使用。
-   - 支持插件与插件接口定义，注意插件共有运行时插件、服务插件、通用插件三种，可以参考官方插件库：https://github.com/pangdogs/plugins，学习如何使用。
+   - 支持组件与组件接口定义，可以参考官方样例：https://git.golaxy.org/examples，学习如何使用。
+   - 支持插件与插件接口定义，注意插件共有运行时插件、服务插件、通用插件三种，可以参考官方样例：https://git.golaxy.org/examples，学习如何使用。
    - 使用Goland作为IDE时，需要更新至2023.2版本以上，否则可能会有误报错。
    - 根据代码结构，选择性使用。
 */
