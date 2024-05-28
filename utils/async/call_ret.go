07461d2f55865af98ca9d5d95390e786addbe852
pangdogs <pangdogs@gmail.com>
2024-05-28 15:49:44 +0800

    修改代码

--- utils/async/call_ret.go
+++ utils/async/call_ret.go
@@ -21,10 +21,32 @@ func MakeRetT[T any](val T, err error) RetT[T] {
 
 // CastRetT 转换
 func CastRetT[T any](ret Ret) RetT[T] {
+	if ret.Value == nil || ret.Error != nil {
+		return RetT[T]{
+			Error: ret.Error,
+		}
+	}
 	return RetT[T]{
 		Value: ret.Value.(T),
 		Error: ret.Error,
 	}
+}
+
+// AsRetT 转换
+func AsRetT[T any](ret Ret) (RetT[T], bool) {
+	if ret.Value == nil || ret.Error != nil {
+		return RetT[T]{
+			Error: ret.Error,
+		}, true
+	}
+	v, ok := ret.Value.(T)
+	if !ok {
+		return RetT[T]{}, false
+	}
+	return RetT[T]{
+		Value: v,
+		Error: ret.Error,
+	}, true
 }
 
 // RetT 调用结果
