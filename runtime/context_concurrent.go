6b02ecd38d3d744ba83dbe05cf8e22b2e74a31df
pangdogs <pangdogs@gmail.com>
2024-05-30 10:35:01 +0800

    修改代码

--- runtime/context_concurrent.go
+++ runtime/context_concurrent.go
@@ -3,7 +3,10 @@ package runtime
 import (
 	"fmt"
 	"git.golaxy.org/core/internal/gctx"
+	"git.golaxy.org/core/service"
 	"git.golaxy.org/core/utils/async"
+	"git.golaxy.org/core/utils/exception"
+	"git.golaxy.org/core/utils/iface"
 	"git.golaxy.org/core/utils/uid"
 )
 
@@ -18,4 +21,30 @@ type ConcurrentContext interface {
 	GetName() string
 	// GetId 获取运行时Id
 	GetId() uid.Id
+}
+
+// Concurrent 获取多线程安全的运行时上下文
+func Concurrent(provider gctx.ConcurrentContextProvider) ConcurrentContext {
+	if provider == nil {
+		panic(fmt.Errorf("%w: %w: provider is nil", ErrContext, exception.ErrArgs))
+	}
+	return iface.Cache2Iface[Context](provider.GetConcurrentContext())
+}
+
+func getServiceContext(provider gctx.ConcurrentContextProvider) service.Context {
+	if provider == nil {
+		panic(fmt.Errorf("%w: %w: provider is nil", ErrContext, exception.ErrArgs))
+	}
+	ctx := iface.Cache2Iface[Context](provider.GetConcurrentContext())
+	if ctx == nil {
+		return nil
+	}
+	return ctx.getServiceCtx()
+}
+
+func getCaller(provider gctx.ConcurrentContextProvider) async.Caller {
+	if provider == nil {
+		panic(fmt.Errorf("%w: %w: provider is nil", ErrContext, exception.ErrArgs))
+	}
+	return iface.Cache2Iface[Context](provider.GetConcurrentContext())
 }
--- runtime/context_current.go
+++ runtime/context_current.go
@@ -3,8 +3,6 @@ package runtime
 import (
 	"fmt"
 	"git.golaxy.org/core/internal/gctx"
-	"git.golaxy.org/core/service"
-	"git.golaxy.org/core/utils/async"
 	"git.golaxy.org/core/utils/exception"
 	"git.golaxy.org/core/utils/iface"
 )
@@ -17,35 +15,9 @@ func Current(provider gctx.CurrentContextProvider) Context {
 	return iface.Cache2Iface[Context](provider.GetCurrentContext())
 }
 
-// Concurrent 获取多线程安全的运行时上下文
-func Concurrent(provider gctx.ConcurrentContextProvider) ConcurrentContext {
-	if provider == nil {
-		panic(fmt.Errorf("%w: %w: provider is nil", ErrContext, exception.ErrArgs))
-	}
-	return iface.Cache2Iface[Context](provider.GetConcurrentContext())
-}
-
-func getCaller(provider gctx.ConcurrentContextProvider) async.Caller {
-	if provider == nil {
-		panic(fmt.Errorf("%w: %w: provider is nil", ErrContext, exception.ErrArgs))
-	}
-	return iface.Cache2Iface[Context](provider.GetConcurrentContext())
-}
-
 func getRuntimeContext(provider gctx.CurrentContextProvider) Context {
 	if provider == nil {
 		panic(fmt.Errorf("%w: %w: provider is nil", ErrContext, exception.ErrArgs))
 	}
 	return iface.Cache2Iface[Context](provider.GetCurrentContext())
-}
-
-func getServiceContext(provider gctx.ConcurrentContextProvider) service.Context {
-	if provider == nil {
-		panic(fmt.Errorf("%w: %w: provider is nil", ErrContext, exception.ErrArgs))
-	}
-	ctx := iface.Cache2Iface[Context](provider.GetConcurrentContext())
-	if ctx == nil {
-		return nil
-	}
-	return ctx.getServiceCtx()
 }
