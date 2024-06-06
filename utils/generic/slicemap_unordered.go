46cc55d9a0c3049e74d4dbc69729fa5b1215014c
pangdogs <pangdogs@gmail.com>
2024-06-06 13:23:36 +0800

    修改代码

---
+++ utils/generic/slicemap_unordered.go
@@ -0,0 +1,113 @@
+package generic
+
+import (
+	"git.golaxy.org/core/utils/types"
+	"slices"
+)
+
+func MakeUnorderedSliceMap[K comparable, V any](kvs ...UnorderedKV[K, V]) UnorderedSliceMap[K, V] {
+	m := make(UnorderedSliceMap[K, V], 0, len(kvs))
+	for i := range kvs {
+		kv := kvs[i]
+		m.Add(kv.K, kv.V)
+	}
+	return m
+}
+
+func NewUnorderedSliceMap[K comparable, V any](kvs ...UnorderedKV[K, V]) *UnorderedSliceMap[K, V] {
+	m := make(UnorderedSliceMap[K, V], 0, len(kvs))
+	for i := range kvs {
+		kv := kvs[i]
+		m.Add(kv.K, kv.V)
+	}
+	return &m
+}
+
+func MakeUnorderedSliceMapFromGoMap[K comparable, V any](m map[K]V) UnorderedSliceMap[K, V] {
+	sm := make(UnorderedSliceMap[K, V], 0, len(m))
+	for k, v := range m {
+		sm.Add(k, v)
+	}
+	return sm
+}
+
+func NewUnorderedSliceMapFromGoMap[K comparable, V any](m map[K]V) *UnorderedSliceMap[K, V] {
+	sm := make(UnorderedSliceMap[K, V], 0, len(m))
+	for k, v := range m {
+		sm.Add(k, v)
+	}
+	return &sm
+}
+
+type UnorderedKV[K comparable, V any] struct {
+	K K
+	V V
+}
+
+type UnorderedSliceMap[K comparable, V any] []UnorderedKV[K, V]
+
+func (m *UnorderedSliceMap[K, V]) Add(k K, v V) {
+	idx := slices.IndexFunc(*m, func(kv UnorderedKV[K, V]) bool {
+		return kv.K == k
+	})
+	if idx >= 0 {
+		(*m)[idx] = UnorderedKV[K, V]{K: k, V: v}
+	} else {
+		*m = append(*m, UnorderedKV[K, V]{K: k, V: v})
+	}
+}
+
+func (m *UnorderedSliceMap[K, V]) TryAdd(k K, v V) bool {
+	idx := slices.IndexFunc(*m, func(kv UnorderedKV[K, V]) bool {
+		return kv.K == k
+	})
+	if idx < 0 {
+		*m = append(*m, UnorderedKV[K, V]{K: k, V: v})
+	}
+	return idx < 0
+}
+
+func (m *UnorderedSliceMap[K, V]) Delete(k K) bool {
+	idx := slices.IndexFunc(*m, func(kv UnorderedKV[K, V]) bool {
+		return kv.K == k
+	})
+	if idx >= 0 {
+		*m = slices.Delete(*m, idx, idx+1)
+	}
+	return idx >= 0
+}
+
+func (m UnorderedSliceMap[K, V]) Get(k K) (V, bool) {
+	idx := slices.IndexFunc(m, func(kv UnorderedKV[K, V]) bool {
+		return kv.K == k
+	})
+	if idx >= 0 {
+		return m[idx].V, true
+	}
+	return types.ZeroT[V](), false
+}
+
+func (m UnorderedSliceMap[K, V]) Value(k K) V {
+	idx := slices.IndexFunc(m, func(kv UnorderedKV[K, V]) bool {
+		return kv.K == k
+	})
+	if idx >= 0 {
+		return m[idx].V
+	}
+	return types.ZeroT[V]()
+}
+
+func (m UnorderedSliceMap[K, V]) Exist(k K) bool {
+	idx := slices.IndexFunc(m, func(kv UnorderedKV[K, V]) bool {
+		return kv.K == k
+	})
+	return idx >= 0
+}
+
+func (m UnorderedSliceMap[K, V]) ToGoMap() map[K]V {
+	gm := make(map[K]V, len(m))
+	for _, kv := range m {
+		gm[kv.K] = kv.V
+	}
+	return gm
+}
